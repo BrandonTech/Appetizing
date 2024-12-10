0ac0a435747e3cb3d74054d1da3e6f47d2b87482
Brandon <brandon@onefunction.tech>
2024-12-10 17:33:47 -0500

    Initial Commit

---
+++ Appetizing/AppetizingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizingApp.swift
+//  Appetizing
+//
+//  Created by Brandon Jadotte on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppetizingUITests/AppetizingUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AppetizingUITestsLaunchTests.swift
+//  AppetizingUITests
+//
+//  Created by Brandon Jadotte on 12/10/24.
+//
+
+import XCTest
+
+final class AppetizingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
